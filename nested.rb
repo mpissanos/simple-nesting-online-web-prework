5a6cc077bb4ce5fe44a13a6a5bcb5c7624ca0c1d
Manny Pissanos <mpissano@gmail.com>
2018-09-20 15:30:57 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -103,7 +103,7 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     programmer_hash.replace[:alan_kay][:known_for] = "GUI"
+     programmer_hash.replace([:alan_kay][:known_for] = "GUI")
      
      
 end
